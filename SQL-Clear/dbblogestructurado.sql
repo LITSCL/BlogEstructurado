8c3f831746922c84c6e97b12a25685d395410a1d
Daniel Álvarez <danielalvarez.litscl.melchiot@protonmail.com>
2023-12-10 18:27:00 -0300

    Ok

--- SQL-Clear/dbblogestructurado.sql
+++ SQL-Clear/dbblogestructurado.sql
@@ -1,9 +1,9 @@
-﻿-- phpMyAdmin SQL Dump
+-- phpMyAdmin SQL Dump
 -- version 5.0.4
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 23-06-2021 a las 21:49:21
+-- Tiempo de generación: 10-12-2023 a las 22:14:37
 -- Versión del servidor: 10.4.17-MariaDB
 -- Versión de PHP: 8.0.0
 
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `dbblogestructurado`
 --
+CREATE DATABASE IF NOT EXISTS `dbblogestructurado` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `dbblogestructurado`;
 
 -- --------------------------------------------------------
 
